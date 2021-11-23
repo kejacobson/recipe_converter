5d29ef643aea16844c0c19627d159237ba7848c6
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2021-11-23 14:25:44 -0500

    Handle multiple pages in a pdf

--- convert_recipes.py
+++ convert_recipes.py
@@ -28,9 +28,27 @@ class RecipeConverter:
 
             for filename in files:
                 image_file = f'{self.folder}/{filename}'
-                word_file = f'{self.word_folder}/{filename}'
-                text = self.read_text_from_image(image_file)
-                self.write_text_to_word_doc(text, word_file, image_file)
+                word_file = f'{self.word_folder}/{self.get_word_file_name(filename)}'
+
+                doc = docx.Document()
+                images, image_files = self.read_images_from_file(image_file)
+                for image in images:
+                    text = self.read_text_from_image(image)
+                    self.write_text_to_word_doc(doc, text)
+
+                for image_file in image_files:
+                    self.write_image_to_word_doc(doc, image_file)
+                doc.save(word_file)
+
+    def read_images_from_file(self, image_file):
+        print('Reading', image_file)
+        if self.filetype_is_pdf(image_file):
+            return self.convert_pdf_to_image(image_file)
+        else:
+            return [cv2.imread(image_file)], [image_file]
+
+    def filetype_is_pdf(self, image_file):
+        return '.pdf' == image_file[-4:]
 
     def remove_ds_store(self):
         ds_store = f'{self.folder}/.DS_Store'
@@ -50,33 +68,28 @@ class RecipeConverter:
         re.sub(u'[^\u0020-\uD7FF\u0009\u000A\u000D\uE000-\uFFFD\U00010000-\U0010FFFF]+', '', line)
 
     def convert_pdf_to_image(self, image_filename):
-        if image_filename[-3:] == 'pdf':
-            images = convert_from_path(image_filename)
-            jpg_file = self.get_file_root(image_filename) +'.jpg'
+        imgs = []
+        image_files = []
+        images = convert_from_path(image_filename)
+        for i, image in enumerate(images):
+            jpg_file = self.get_file_root(image_filename) +f'{i}.jpg'
             images[0].save(jpg_file, 'JPEG')
-            return jpg_file
-        else:
-            return image_filename
-        
-    def read_text_from_image(self, image_filename):
-        image_filename = self.convert_pdf_to_image(image_filename)
-        print('Reading', image_filename)
-        img = cv2.imread(image_filename)
-        text:str = pytesseract.image_to_string(img)
+            imgs.append(cv2.imread(jpg_file))
+            image_files.append(jpg_file)
+        return imgs, image_files
+
+    def read_text_from_image(self, image):
+        text = pytesseract.image_to_string(image)
+        re.sub('\n\n','\n',text)
         return text
 
-    def write_text_to_word_doc(self, text, image_filename, full_image_path):
-        if image_filename[-3:] == 'pdf':
-            full_image_path = self.convert_pdf_name_to_image(full_image_path)
-        doc = docx.Document()
-        re.sub('\n\n','\n',text)
+    def write_text_to_word_doc(self, doc, text):
         for line in text.splitlines():
             self.make_string_xml_compatible(line)
             doc.add_paragraph(line)
-        doc.add_picture(full_image_path, width=docx.shared.Inches(7))
-        word_file = self.get_word_file_name(image_filename)
-        doc.save(word_file)
 
+    def write_image_to_word_doc(self, doc, image_path):
+        doc.add_picture(image_path, width=docx.shared.Inches(7))
 
 if __name__ == '__main__':
     converter = RecipeConverter()
